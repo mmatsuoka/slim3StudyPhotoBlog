155cf8a8f3700c6f14ec1343af90d1ec8527b26a
Masayasu Matsuoka <matsuoka.m@gmail.com>
2011-06-10 21:15:19 +0900

    キーを保持するクラスを追加。

---
+++ src/com/mprog/photoBlog/controller/bbs/RequestKeys.java
@@ -0,0 +1,13 @@
+package com.mprog.photoBlog.controller.bbs;
+
+public interface RequestKeys {
+
+    public static final String PASSWORD = "password";
+    public static final String USERNAME = "username";
+    public static final String TITLE = "title";
+    public static final String PHOTO = "photo";
+    public static final String HEAD_LIST = "headList";
+    public static final String KEY = "key";
+    public static final String MESSAGE = "message";
+
+}
---
+++ src/com/mprog/photoBlog/controller/bbs/SessionKeys.java
@@ -0,0 +1,7 @@
+package com.mprog.photoBlog.controller.bbs;
+
+public class SessionKeys {
+
+    public static final String AUTH_USER = "authUser";
+
+}
